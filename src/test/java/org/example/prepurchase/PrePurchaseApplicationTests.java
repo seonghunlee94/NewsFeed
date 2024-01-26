c304408f143c9e6a888d411b5737733305df3d01
seonghunlee94 <godloveme94@naver.com>
2024-01-26 16:46:57 +0900

    Integrating DBMS after creating a project

---
+++ src/test/java/org/example/prepurchase/PrePurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prepurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrePurchaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
