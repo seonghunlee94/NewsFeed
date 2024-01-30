32c38ae29cf9fdbd096301447992f360def13432
seonghunlee94 <godloveme94@naver.com>
2024-01-30 19:43:31 +0900

    Apply membership and validation to Users and UserRepository.

--- src/main/java/org/example/prepurchase/domain/user/dao/UserRepository.java
+++ src/main/java/org/example/prepurchase/domain/user/dao/UserRepository.java
@@ -7,13 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-    // 추가적인 쿼리 메서드가 필요하다면 여기에 선언할 수 있습니다.
-
-    // 이메일을 사용하여 사용자 조회
     Users findByEmail(String email);
 
-    // 추가적인 쿼리 메서드 예시:
-    // Users findByUsername(String username);
-    // List<Users> findByAgeGreaterThan(int age);
-    // 등등...
 }
