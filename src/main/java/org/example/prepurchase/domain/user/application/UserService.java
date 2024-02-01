412ada997a5da8587a6fa23cd916835eb03d33a2
seonghunlee94 <godloveme94@naver.com>
2024-02-01 23:39:19 +0900

    Implement Login, add a column to the Users table

--- src/main/java/org/example/prepurchase/domain/user/application/UserService.java
+++ src/main/java/org/example/prepurchase/domain/user/application/UserService.java
@@ -1,10 +1,14 @@
 package org.example.prepurchase.domain.user.application;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.example.prepurchase.domain.user.domain.Users;
 import org.example.prepurchase.domain.user.dao.UserRepository;
+import org.example.prepurchase.domain.user.dto.LoginRequestDto;
 import org.example.prepurchase.domain.user.dto.UserRequestDto;
 import org.example.prepurchase.domain.user.exception.DuplicateEmailException;
 import org.example.prepurchase.domain.user.exception.DuplicateUsernameException;
+import org.example.prepurchase.global.auth.UserRoleEnum;
+import org.example.prepurchase.global.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,11 +19,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+
     }
 
 
@@ -45,10 +52,31 @@ public class UserService {
                 newUserDto.getEmail(),
                 encodedPassword,
                 newUserDto.getGreeting(),
-                newUserDto.getProfileImage()
+                newUserDto.getProfileImage(),
+                UserRoleEnum.USER
         );
 
         return userRepository.save(newUser);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 사용자 확인
+        Users user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new IllegalArgumentException("등록된 사용자가 없습니다.");
+        }
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token, res);
     }
 
 
--- src/main/java/org/example/prepurchase/global/config/SecurityConfig.java
+++ src/main/java/org/example/prepurchase/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,11 +26,20 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
                                 .requestMatchers(
-                                        "/users/signup"
+                                        "/users/signup",
+                                        "/users/login",
+                                        "/api/create-jwt",
+                                        "/api/get-jwt"
                                 )
                                 .permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                ;
 
 
         return http.build();
