20ad3d37c3c3e604d24e7e86482fbea779cbec95
seonghunlee94 <godloveme94@naver.com>
2024-02-01 23:50:33 +0900

    Clean up the signup code and rename the class.

--- src/main/java/org/example/prepurchase/domain/user/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package org.example.prepurchase.domain.user.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
