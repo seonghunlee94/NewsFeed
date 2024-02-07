e2c7f94b8d1415ce3e52d9f800d279eef03cc427
seonghunlee94 <godloveme94@naver.com>
2024-02-08 02:51:20 +0900

    Implement newsfeed creation and viewing.

--- src/main/java/org/example/prepurchase/domain/comment/application/CommentService.java
+++ src/main/java/org/example/prepurchase/domain/comment/application/CommentService.java
@@ -1,12 +1,16 @@
 package org.example.prepurchase.domain.comment.application;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.prepurchase.domain.comment.dao.CommentLoveRepository;
 import org.example.prepurchase.domain.comment.dao.CommentRepository;
 import org.example.prepurchase.domain.comment.domain.CommentLove;
 import org.example.prepurchase.domain.comment.domain.Comments;
 import org.example.prepurchase.domain.comment.dto.CreateCommentRequestDto;
+import org.example.prepurchase.domain.newsfeed.application.NewsFeedService;
 import org.example.prepurchase.domain.post.dao.PostRepository;
 import org.example.prepurchase.domain.post.domain.Posts;
+import org.example.prepurchase.global.config.NewsFeedType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,14 +26,24 @@ public class CommentService {
     private final PostRepository postRepository;
     private final CommentLoveRepository commentLoveRepository;
 
+    // 모듈 작업 후 삭제
+    private final NewsFeedService newsFeedService;
 
-    public CommentService(CommentRepository commentRepository, PostRepository postRepository, CommentLoveRepository commentLoveRepository) {
+
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository, CommentLoveRepository commentLoveRepository, NewsFeedService newsFeedService) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
         this.commentLoveRepository = commentLoveRepository;
+        this.newsFeedService = newsFeedService;
     }
 
-    public void createComment(String idString, String username, CreateCommentRequestDto createComment) {
+    public void createComment(HttpServletRequest request, CreateCommentRequestDto createComment) {
+
+        String idString = request.getHeader("id");
+        String userId = request.getHeader("userId");
+        String username = request.getHeader("username");
+        String title = createComment.getTitle();
+
         Long id = Long.valueOf(idString);
         LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
 
@@ -49,6 +63,12 @@ public class CommentService {
         comment.setUpdateTime(dateTime);
 
         commentRepository.save(comment);
+
+        // 추후에 모듈화 작업할 때, 호출 방식 바꾸기.
+        if (!userId.equals(username)) {
+            newsFeedService.createNewsFeed(title, userId, username, NewsFeedType.COMMENT);
+        }
+
 
     }
 
---
+++ src/main/java/org/example/prepurchase/global/config/NewsFeedType.java
@@ -0,0 +1,7 @@
+package org.example.prepurchase.global.config;
+
+public enum NewsFeedType {
+    FOLLOW,
+    POST_LOVE,
+    COMMENT
+}
