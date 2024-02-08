44da9ea8dce8a7765c0a11df80a04b6db7d1e4ed
seonghunlee94 <godloveme94@naver.com>
2024-02-08 14:32:50 +0900

    Monolithize the user_service.

--- user_service/src/main/java/org/example/prepurchase/domain/newsfeed/dao/NewsFeedQRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.prepurchase.domain.newsfeed.dao;
-
-import org.example.prepurchase.domain.newsfeed.domain.NewsFeeds;
-
-import java.util.List;
-
-public interface NewsFeedQRepository {
-    List<NewsFeeds> getNewsFeeds(String receiverId);
-}
--- user_service/src/main/java/org/example/prepurchase/global/config/NewsFeedType.java
+++
@@ -1,7 +0,0 @@
-package org.example.prepurchase.global.config;
-
-public enum NewsFeedType {
-    FOLLOW,
-    POST_LOVE,
-    COMMENT
-}
