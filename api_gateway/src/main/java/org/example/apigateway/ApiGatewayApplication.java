617f77bc2eea35daaec56e41cb346fecd9b52e2f
seonghunlee94 <godloveme94@naver.com>
2024-02-14 19:59:55 +0900

    Create a Eureka server and an api-gateway to route the MSA service URL.

--- api_gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package org.example.prepurchase;
+package org.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableDiscoveryClient
-public class NewsFeedApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NewsFeedApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
     
 }
