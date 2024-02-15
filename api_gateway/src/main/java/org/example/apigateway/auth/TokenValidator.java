168cf2e5929998583c88ee1282b9bdc263738353
seonghunlee94 <godloveme94@naver.com>
2024-02-15 13:17:43 +0900

    Delete or ignore unnecessary code and directories.

--- api_gateway/src/main/java/org/example/apigateway/auth/TokenValidator.java
+++ api_gateway/src/main/java/org/example/apigateway/auth/TokenValidator.java
@@ -7,34 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.security.Key;
 import java.util.Base64;
-import java.util.Date;
 
 
 @Component
 public class TokenValidator {
 
-    // Header KEY 값
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String REFRESH_HEADER = "RefreshToken";
-    // 사용자 권한 값의 KEY
-    public static final String AUTHORIZATION_KEY = "auth";
-    // Token 식별자
-    public static final String BEARER_PREFIX = "Bearer ";
-    // 토큰 만료시간
-    private final long ACCESS_TOKEN_TIME = 10 * 60 * 1000L; // 10분
-    private final long REFRESH_TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
     @Value("${jwt.secret}") // Base64 Encode 한 SecretKey
     private String secretKey;
 
     private Key key;
-    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
 
     // 로그 설정
     public static final Logger logger = LoggerFactory.getLogger("JWT 관련 로그");
--- user_service/src/main/java/org/example/prepurchase/global/util/JwtUtil.java
+++ user_service/src/main/java/org/example/prepurchase/global/util/JwtUtil.java
@@ -71,11 +71,10 @@ public class JwtUtil {
     // JWT Cookie 에 저장
     public void addJwtToCookie(String token, HttpServletResponse res, String header) {
         try {
-//            logger.info(token);
-            token = URLEncoder.encode(token, "utf-8").replaceAll("\\+", "%"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
-//            logger.info(token);
+            // Cookie Value 에는 공백이 불가능해서 encoding 진행
+            token = URLEncoder.encode(token, "utf-8").replaceAll("\\+", "%");
 
-            Cookie cookie = new Cookie(header, token); // Name-Value
+            Cookie cookie = new Cookie(header, token);
             cookie.setPath("/");
 
             // Response 객체에 Cookie 추가
@@ -85,7 +84,7 @@ public class JwtUtil {
         }
     }
 
-    // JWT 토큰 substring -- 나중에 확인하기.
+    // JWT 토큰 substring
     public String substringToken(String tokenValue) {
         if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
             return tokenValue.substring(7);
@@ -94,7 +93,7 @@ public class JwtUtil {
         throw new NullPointerException("Not Found Token");
     }
 
-    // 토큰 검증 -- 나중에 확인하기.
+    // 토큰 검증
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
@@ -111,7 +110,7 @@ public class JwtUtil {
         return false;
     }
 
-    // 토큰에서 사용자 정보 가져오기 -- 나중에 확인하기.
+    // 토큰에서 사용자 정보 가져오기
     public Claims getUserInfoFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
     }
