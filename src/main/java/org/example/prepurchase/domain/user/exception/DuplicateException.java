0ba48326db85a4a43df0686cf07deb27587c31f0
seonghunlee94 <godloveme94@naver.com>
2024-03-04 23:15:57 +0900

    Delete unnecessary code

--- src/main/java/org/example/prepurchase/domain/user/exception/DuplicateException.java
+++
@@ -1,7 +0,0 @@
-package org.example.prepurchase.domain.user.exception;
-
-public class DuplicateException extends RuntimeException {
-    public DuplicateException(String message) {
-        super(message);
-    }
-}
